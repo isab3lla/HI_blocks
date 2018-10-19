c34885349c6764959d09c5e9d627440b63aab721
Isabella C <isab3lla.c@gmail.com>
2018-10-19 12:26:37 +0100

    Created a readme

---
+++ HI/README.m
@@ -0,0 +1,16 @@
+## keeping track of these files
+
+# HI_hybrid_lib.py: 
+method for populating the sims (halos) with HI.
+Plus a repository of the available sims.
+
+# HI_Pk_from_HIblock.py:
+calculating the HI Pk reading the brand new HI 
+block directly from the modified snapshot!
+
+
+# HI_Pk.py: old style. Read Fof halos, assing HI, calculate Pk.
+
+
+# looping_for_OmegaHI_Ulysses.py
+just computing the Omega_HI
